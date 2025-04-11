5ed2f2551ee30e76fb8e53228a720adac57c59fc
erick <e.kanja@alustudent.com>
2025-04-11 13:09:35 +0200

    Add logic to select an option in the main menu

--- contact_book.py
+++ contact_book.py
@@ -16,21 +16,21 @@ cursor = db.cursor()
 
 class Contact:
     def __init__(self):
-        self.name = input("Enter the name:")
-        self.phone = int(input("Enter the phone number:"))
-        self.email= input("Enter the email:")
+        self.name = input("Enter the name: ")
+        self.phone = int(input("Enter the phone number: "))
+        self.email= input("Enter the email: ")
         insert_data_query = "INSERT INTO contact_details (name, phone_number, email) VALUES (%s, %s, %s)"
         self.values = (self.name, self.phone, self.email)
         cursor.execute(insert_data_query, self.values)
         db.commit()
 
 def view_all_contacts(cursor):
-    view_all_query = "SELECT * FROM contact_details"
+    view_all_query = "SELECT name, phone_number, email FROM contact_details"
     cursor.execute(view_all_query)
     return cursor.fetchall()
 
 def view_contact(cursor, name):
-    view_contact_query = "SELECT * FROM contact_details WHERE name=%s"
+    view_contact_query = "SELECT name, phone_number, email FROM contact_details WHERE name=%s"
     value = (name,)
     cursor.execute(view_contact_query, value)
     return cursor.fetchone()
@@ -45,6 +45,23 @@ def delete_contact(cursor, name):
 
 
 # contact1 = Contact()
-print(view_all_contacts(cursor))
-print(delete_contact(cursor, 'Erick'))
-print(view_all_contacts(cursor))
+# print(view_all_contacts(cursor))
+# print(delete_contact(cursor, 'Erick'))
+# print(view_all_contacts(cursor))
+# print(view_all_contacts(cursor))
+while True:
+    a = int(input("Welcome to the Contact Book. Select and option\n1.) Add a contact\n2.) View all contacts\n3.) Search a contact by name\n4.) Delete a contact\n"))
+    if a == 1:
+        contact1 = Contact()
+    elif a == 2:
+        all_contacts = view_all_contacts(cursor)
+        for contact in all_contacts:
+            print(contact)
+    elif a == 3:
+        contact_name = input("Whose contact would you like? ")
+        searched_contact = view_contact(cursor, contact_name)
+        for contact in searched_contact:
+            print(contact)
+    elif a == 4:
+        deleted_contact = input("Which contact would you like to delete?")
+        delete_contact(cursor, deleted_contact)
